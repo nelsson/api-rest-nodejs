49cd8f01c97fa72dd1f5805c9c56b93b4ff839e5
Nelson <nelsson@gmail.com>
2020-02-25 12:32:42 -0500

    change config.js url mongodb

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ module.exports = {
   port: process.env.PORT || 3001,
   db:
     process.env.MONGODB ||
-    "mongodb://localhost:27017/shop" ||
     "mongodb://heroku_rfksf02t:5imvltld2foavcq2dloq1dbijb@ds033317.mlab.com:33317/heroku_rfksf02t",
   SECRET_TOKEN: "miclavedetokens"
 };
